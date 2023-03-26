0b58c23c7f6582a1d815647fba1d17013d97189a
Nikhil Dubey <126849921+nikhildubey165@users.noreply.github.com>
2023-03-26 17:46:17 +0000

    main

---
+++ introductory_control_P1.py
@@ -0,0 +1,13 @@
+'''
+Problem Statement- I
+Design and write a program to calculate Net Run Rate scored by the two teams use controls to validate a condition such that the team with the higher run rate will top the points table and also think for the tie-breaker condition.
+
+Steps to Follow: 
+Understand how net run rate getting calculated (Cite the reference in submission comment)
+Follow the computational thinking process.
+Understand inputs required to calculate various parameters (Give Appropriate Comments to the code)
+Design Controls -  Compound Statements and Suites
+Provide Feedback to the User (Console level Feedback)
+Test (Paper Calculation)
+Validate (Paper Calculation = Code  Result)
+'''
---
+++ introductory_control_P2.py
@@ -0,0 +1,13 @@
+'''
+Problem Statement- II
+We have three categories to check. If the sum of divisors is greater than a number, the number is
+abundant. If the sum of divisors is less than a number, the number is deficient. Otherwise, it must
+be perfect design control structure for this problem statement
+
+#Nint=28 # Number to be validated 
+#Div=1    #Divisor
+#while Div<Nint:
+#   if Nint % Div==0:
+#        print(Div) # Suit1
+#Div=Div+1  # Suit 2
+'''
